641a63128172deafbadab865efeabe87a99006dd
Rizkyard17 <rizkyjambi859@gmail.com>
2025-06-09 00:07:55 +0700

    edit migration for deploy

--- database/migrations/2025_06_08_233625_create_sessions_table.php
+++ database/migrations/2025_06_08_233625_create_sessions_table.php
@@ -4,19 +4,24 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-return new class extends Migration {
-    public function up(): void {
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->text('payload');
-            $table->integer('last_activity')->index();
-        });
+class CreateSessionsTable extends Migration
+{
+    public function up()
+    {
+        if (!Schema::hasTable('sessions')) {
+            Schema::create('sessions', function (Blueprint $table) {
+                $table->string('id')->primary();
+                $table->unsignedBigInteger('user_id')->nullable();
+                $table->string('ip_address', 45)->nullable();
+                $table->text('user_agent')->nullable();
+                $table->text('payload');
+                $table->integer('last_activity');
+            });
+        }
     }
 
-    public function down(): void {
+    public function down()
+    {
         Schema::dropIfExists('sessions');
     }
-};
+}
